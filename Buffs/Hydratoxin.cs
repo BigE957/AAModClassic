20dee6ec4a58ff47414f501507fb4c77a8553832
Big E <evanmorrison42@gmail.com>
2026-03-03 15:19:24 -0500

    + Porting Progress 9: 7524 Errors.

--- Buffs/Hydratoxin.cs
+++ Buffs/Hydratoxin.cs
@@ -1,6 +1,7 @@
 using AAModClassic;
 using AAModClassic.Globals;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace AAModClassic.Buffs
@@ -14,10 +15,10 @@ namespace AAModClassic.Buffs
 			Main.debuff[Type] = true;
 			Main.pvpBuff[Type] = true;
 			Main.buffNoSave[Type] = true;
-			longerExpertDebuff/* tModPorter Note: Removed. Use BuffID.Sets.LongerExpertDebuff instead */ = true;
-		}
+            BuffID.Sets.LongerExpertDebuff[Type] = true;
+        }
 
-		public override void Update(Player player, ref int buffIndex)
+        public override void Update(Player player, ref int buffIndex)
 		{
             player.GetModPlayer<AAPlayer>().hydraToxin = true;
         }
