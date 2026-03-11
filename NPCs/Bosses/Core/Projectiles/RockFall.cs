d258f648f297870a289ec87260dd4918e51b6582
Big E <evanmorrison42@gmail.com>
2026-03-11 01:53:20 -0400

    + Initial Biomite Core Code Port

---
+++ NPCs/Bosses/Core/Projectiles/RockFall.cs
@@ -0,0 +1,28 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AAModClassic.NPCs.Bosses.Core.Projectiles;
+
+public class RockFall : ModProjectile
+{
+	public override string Texture => "AAModClassic/BlankTex";
+
+	public override void SetDefaults()
+	{
+		Projectile.width = 250;
+		Projectile.height = 2;
+		Projectile.hostile = true;
+		Projectile.penetrate = 1;
+		Projectile.ignoreWater = true;
+		Projectile.tileCollide = true;
+	}
+
+	public override void AI()
+	{
+		Projectile.ai[0] += 1f;
+		if (Main.npc[(int)Projectile.ai[0]].ai[1] != 2f)
+		{
+			((Entity)Projectile).active = false;
+		}
+	}
+}
