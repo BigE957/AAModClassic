c9b2374d17b90e3aec08e4df46e0601cff6ecabf
Big E <evanmorrison42@gmail.com>
2026-04-12 01:25:15 -0400

    + EX Soul Map Layer Drawing

--- Items/Boss/EXSoul.cs
+++ Items/Boss/EXSoul.cs
@@ -1,7 +1,12 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
-using Microsoft.Xna.Framework;
+using Terraria.Map;
+using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace AAModClassic.Items.Boss
 {
@@ -14,6 +19,7 @@ namespace AAModClassic.Items.Boss
             // ticksperframe, frameCount
             Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(4, 4));
             ItemID.Sets.ItemNoGravity[Item.type] = true;
+            ItemID.Sets.AnimatesAsSoul[Type] = true;
         }
 
         // TODO -- Velocity Y smaller, post NewItem?
@@ -40,6 +46,30 @@ namespace AAModClassic.Items.Boss
         public override void PostUpdate()
         {
             Lighting.AddLight(Item.Center, Main.DiscoColor.ToVector3() * 0.55f * Main.essScale);
+        }
+    }
+
+    public class EXSoulMapLayer : ModMapLayer
+    {
+        public override Position GetDefaultPosition() => new After(IMapLayer.Pings);
+
+        public override void Draw(ref MapOverlayDrawContext context, ref string text)
+        {
+            foreach (Item item in Main.ActiveItems)
+            {
+                if (item.type != ModContent.ItemType<EXSoul>())
+                    continue;
+
+                Texture2D tex = TextureAssets.Item[ModContent.ItemType<EXSoul>()].Value;
+
+                Vector2 tilePosition = item.position / 16f;
+
+                int animTime = (int)(Main.GlobalTimeWrappedHourly * 15);
+
+                var v = context.Draw(tex, tilePosition, Main.DiscoColor, new SpriteFrame(1, 4, 0, (byte)(animTime % 4)), 1.25f, 1.25f, Alignment.Center);
+                if(v.IsMouseOver)
+                    text = item.Name;
+            }
         }
     }
 }
