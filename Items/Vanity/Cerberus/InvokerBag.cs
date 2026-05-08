1f3e41821e897f7d3906b7de3b97c189b5e009c7
Big E <evanmorrison42@gmail.com>
2026-05-07 23:51:30 -0400

    + Error snipes

--- Items/Vanity/Cerberus/InvokerBag.cs
+++ Items/Vanity/Cerberus/InvokerBag.cs
@@ -1,5 +1,5 @@
 using AAModClassic._Content._Dev.__Hardmode.Items.Pets;
-using AAModClassic.Items.Dev.DevTile.Tiles;
+using AAModClassic._Content._Dev.World.Tiles;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
