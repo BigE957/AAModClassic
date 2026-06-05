4062b17908173ebdf166d34f6fa34573e4652be5
Big E <evanmorrison42@gmail.com>
2026-06-05 14:54:11 -0400

    + missing texture fix

--- _Content/Void/_PostMoonlord/Items/_BossZero/Weapons/RiftShredder_Rift.cs
+++ _Content/Void/_PostMoonlord/Items/_BossZero/Weapons/RiftShredder_Rift.cs
@@ -47,7 +47,7 @@ namespace AAModClassic._Content.Void._PostMoonlord.Items._BossZero.Weapons
         public override bool PreDraw(ref Color lightColor)
         {
             Texture2D Tex = TextureAssets.Projectile[Projectile.type].Value;
-            Texture2D Vortex = ModContent.Request<Texture2D>(ModContent.GetInstance<SingularityCannon_SingularityVortex>().Texture + "_Back").Value;
+            Texture2D Vortex = ModContent.Request<Texture2D>(ModContent.GetInstance<SingularityCannon_SingularityVortex>().Texture + "Back").Value;
             Rectangle frame = new Rectangle(0, 0, Tex.Width, Tex.Height);
             BaseDrawing.DrawTexture(Main.spriteBatch, Vortex, 0, Projectile.position, Projectile.width, Projectile.height, Projectile.scale, RingRotation, 0, 1, frame, Projectile.GetAlpha(ColorUtils.COLOR_GLOWPULSE), true);
             BaseDrawing.DrawTexture(Main.spriteBatch, Tex, 0, Projectile.position, Projectile.width, Projectile.height, Projectile.scale, -RingRotation, 0, 1, frame, Projectile.GetAlpha(ColorUtils.COLOR_GLOWPULSE), true);
