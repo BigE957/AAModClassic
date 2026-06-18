12c2a8b203bb11bdb4a2d5651c1683b697b109bd
Big E <evanmorrison42@gmail.com>
2026-06-18 17:26:57 -0400

    + Baron bunny fixes

--- _Content/Bunny/_PostMoonlord/Items/Armor/ChampionHelmetSummoner_BaronBunny.cs
+++ _Content/Bunny/_PostMoonlord/Items/Armor/ChampionHelmetSummoner_BaronBunny.cs
@@ -286,10 +286,10 @@ namespace AAModClassic._Content.Bunny._PostMoonlord.Items.Armor
             Player player = Main.player[Projectile.owner];
 
             Texture2D tex = TextureAssets.Projectile[Projectile.type].Value;
-            Texture2D Mad = ModContent.Request<Texture2D>("AAModClassic/Items/Armor/Champion/Baron/BaronMad").Value;
-            Texture2D Eyes = ModContent.Request<Texture2D>("AAModClassic/Items/Armor/Champion/Baron/BaronMadEyes").Value;
-            Texture2D Meter = ModContent.Request<Texture2D>("AAModClassic/Items/Armor/Champion/Baron/BaronBunnyMeter").Value;
-            Texture2D MeterGlow = ModContent.Request<Texture2D>(Texture + "_Glow").Value;
+            Texture2D Mad = ModContent.Request<Texture2D>(Texture + "Mad").Value;
+            Texture2D Eyes = ModContent.Request<Texture2D>(Texture + "Eyes").Value;
+            Texture2D Meter = ModContent.Request<Texture2D>(Texture + "BunnyMeter").Value;
+            Texture2D MeterGlow = ModContent.Request<Texture2D>(Texture + "BunnyMeter_Glow").Value;
 
             Rectangle frame = BaseDrawing.GetFrame(Projectile.frame, tex.Width, tex.Height / 8, 0, 0);
             Rectangle MeterFrame = BaseDrawing.GetFrame(MeterF, Meter.Width, Meter.Height / 5, 0, 0);
