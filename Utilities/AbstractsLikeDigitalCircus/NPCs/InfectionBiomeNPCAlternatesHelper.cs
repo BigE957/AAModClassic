428823888a09147955bb4daaae9b30928695cd45
Big E <evanmorrison42@gmail.com>
2026-06-19 13:26:04 -0400

    + usings

--- Utilities/AbstractsLikeDigitalCircus/NPCs/InfectionBiomeNPCAlternatesHelper.cs
+++ Utilities/AbstractsLikeDigitalCircus/NPCs/InfectionBiomeNPCAlternatesHelper.cs
@@ -2,13 +2,7 @@
 using ReLogic.Content;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.GameContent;
 using Terraria.ModLoader;
 
 namespace AAModClassic.Utilities.AbstractsLikeDigitalCircus.NPCs
