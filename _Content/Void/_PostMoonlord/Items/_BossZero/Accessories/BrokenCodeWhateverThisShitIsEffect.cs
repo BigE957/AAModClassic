e1aebe6ea6ce467161a6e52e6a7835f930730ecb
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2026-06-20 02:00:13 -0500

    the broken code 2

---
+++ _Content/Void/_PostMoonlord/Items/_BossZero/Accessories/BrokenCodeWhateverThisShitIsEffect.cs
@@ -0,0 +1,40 @@
+﻿using AAModClassic._Content.Terra.__Hardmode.Items.Armor;
+using AAModClassic._Unreleased.Content.Void.Dusts;
+using AAModClassic.Dusts;
+using AAModClassic.Globals;
+using AAModClassic.Rarities;
+using AAModClassic.UI.World;
+using AAModClassic.Utilities.AbstractsLikeDigitalCircus;
+using AAModClassic.Utilities.AbstractsLikeDigitalCircus.Items;
+using Humanizer;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAModClassic._Content.Void._PostMoonlord.Items._BossZero.Accessories
+{
+    public class BrokenCodeWhateverThisShitIsEffect : EquipmentEffectData
+    {
+        public override void DoEffect(Player player)
+        {
+            player.GetModPlayer<BrokenCodeWhateverThisShitIsPlayer>().effect = true;
+        }
+    }
+
+    public class BrokenCodeWhateverThisShitIsPlayer : EquipEffectAbstract
+    {
+        public override void OnHitByAnything(Player.HurtInfo hurtInfo, NPC npc = null, Projectile proj = null)
+        {
+            if (effect)
+            {
+                Player.AddBuff(BuffID.Panic, 180);
+                Player.immuneTime = Player.longInvince ? 180 : 120;
+            }
+            ;
+        }
+    }
+}
