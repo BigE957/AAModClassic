86f44a9fac19849d1d0272cb3c72b2484b31ec91
Big E <evanmorrison42@gmail.com>
2026-06-30 14:16:10 -0400

    + Calamity Debuff Display Support

--- Buffs/Impaled_Buff.cs
+++ Buffs/Impaled_Buff.cs
@@ -23,11 +23,11 @@ namespace AAModClassic.Buffs
                 npc.lifeRegen = 0;
             int JavelinCount = 0;
             int impaleDamage = 0;
-            for (int i = 0; i < 1000; i++)
+            foreach(Projectile p in Main.ActiveProjectiles)
             {
-                if (Main.projectile[i].active && Main.projectile[i].GetGlobalProjectile<ImplaingProjectile>().CanImpale && ((Main.projectile[i].ai[0] == 1f && Main.projectile[i].ai[1] == npc.whoAmI) || (Main.projectile[i].type == ModContent.ProjectileType<OreChunk>() && Main.projectile[i].ai[0] == 1f && Main.projectile[i].ai[1] == ItemID.TungstenOre && Main.projectile[i].localAI[1] == npc.whoAmI)))
+                if (p.active && p.GetGlobalProjectile<ImplaingProjectile>().CanImpale && ((p.ai[0] == 1f && p.ai[1] == npc.whoAmI) || (p.type == ModContent.ProjectileType<OreChunk>() && p.ai[0] == 1f && p.ai[1] == ItemID.TungstenOre && p.localAI[1] == npc.whoAmI)))
                 {
-                    impaleDamage += Main.projectile[i].GetGlobalProjectile<ImplaingProjectile>().damagePerImpaler;
+                    impaleDamage += p.GetGlobalProjectile<ImplaingProjectile>().damagePerImpaler;
                     JavelinCount++;
                 }
             }
