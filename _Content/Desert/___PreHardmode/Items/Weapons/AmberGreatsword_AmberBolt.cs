0d9aa83d17d13e4c2bf56fef55f063f21491f7ea
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2026-07-08 17:16:07 -0500

    gems no loner glow

--- _Content/Desert/___PreHardmode/Items/Weapons/AmberGreatsword_AmberBolt.cs
+++ _Content/Desert/___PreHardmode/Items/Weapons/AmberGreatsword_AmberBolt.cs
@@ -12,7 +12,7 @@ namespace AAModClassic._Content.Desert.___PreHardmode.Items.Weapons
 {
     public class AmberGreatsword_AmberBolt : DiamondGreatsword_DiamondBolt
     {
-        public override Color LightColor => new Color((255 - Projectile.alpha) * 1f / 255f, (255 - Projectile.alpha) * 0.7f / 255f, (255 - Projectile.alpha) * 0f / 255f);
+        public override (float, float, float) LightColor => ((255 - Projectile.alpha) * 1f / 255f, (255 - Projectile.alpha) * 0.7f / 255f, (255 - Projectile.alpha) * 0f / 255f);
         public override Color DustColor => Color.Orange;
         public override Color DrawColor => Color.White;
 
--- _Content/Underground/___PreHardmode/Items/Weapons/EmeraldGreatsword_EmeraldBolt.cs
+++ _Content/Underground/___PreHardmode/Items/Weapons/EmeraldGreatsword_EmeraldBolt.cs
@@ -11,7 +11,7 @@ namespace AAModClassic._Content.Underground.___PreHardmode.Items.Weapons
 {
     public class EmeraldGreatsword_EmeraldBolt : DiamondGreatsword_DiamondBolt
     {
-        public override Color LightColor => new Color((255 - Projectile.alpha) * 0f / 255f, (255 - Projectile.alpha) * 1f / 255f, (255 - Projectile.alpha) * .4f / 255f);
+        public override (float, float, float) LightColor => ((255 - Projectile.alpha) * 0f / 255f, (255 - Projectile.alpha) * 1f / 255f, (255 - Projectile.alpha) * .4f / 255f);
         public override Color DustColor => Color.LightSeaGreen;
         public override Color DrawColor => Color.White;
 
