1d06e6601ef232addb67c1566b763d3ba6746250
Big E <evanmorrison42@gmail.com>
2026-07-12 21:32:43 -0400

    + Added items for the Data Bank and Ancient Data Bank

---
+++ _Removed/Content/Parthenan/__Hardmode/Items/Tiles/Decoration/DataBank.cs
@@ -0,0 +1,47 @@
+﻿using AAModClassic._Removed.Content.Parthenan.__Hardmode.Items.Materials;
+using AAModClassic._Removed.Content.Parthenan.__Hardmode.Items.Tiles.Decoration.Ancient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAModClassic._Removed.Content.Parthenan.__Hardmode.Items.Tiles.Decoration
+{
+    public class DataBank : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables";
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Data Bank");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.rare = ItemRarityID.Pink;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 500;
+            Item.createTile = ModContent.TileType<DataBank_Tile>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<FulguriteBar>(), 5);
+            recipe.AddIngredient(ItemID.Book, 10);
+            recipe.AddTile(TileID.Sawmill);
+            recipe.Register();
+        }
+    }
+}
