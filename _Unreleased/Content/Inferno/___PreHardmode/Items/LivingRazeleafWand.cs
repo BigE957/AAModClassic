0d53e60f40c573582fca04eb7487d7b62162eda2
Big E <evanmorrison42@gmail.com>
2026-07-17 00:20:56 -0400

    + Added the Living Razewood Wand, Living Razeleaf Wand, Living Bogwood Wand, and Living Bogleaf Wand

---
+++ _Unreleased/Content/Inferno/___PreHardmode/Items/LivingRazeleafWand.cs
@@ -0,0 +1,23 @@
+﻿using AAModClassic._Content.Inferno.___PreHardmode.Items.Tiles.Decoration;
+using AAModClassic._Content.Inferno.World.Tiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAModClassic._Unreleased.Content.Inferno.___PreHardmode.Items
+{
+    public class LivingRazeleafWand : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Tools";
+        public override void SetStaticDefaults() => ItemID.Sets.DisableAutomaticPlaceableDrop[Type] = true;
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.LivingWoodWand);
+
+            Item.tileWand = ModContent.ItemType<Razewood>();
+            Item.createTile = ModContent.TileType<LivingRazeleaves_Tile>();
+        }
+    }
+}
---
+++ _Unreleased/Content/Mire/___PreHardmode/LivingBogleafWand.cs
@@ -0,0 +1,22 @@
+﻿using AAModClassic._Content.Mire.___PreHardmode.Items.Tiles.Decoration;
+using AAModClassic._Content.Mire.World.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAModClassic._Unreleased.Content.Mire.___PreHardmode
+{
+    public class LivingBogleafWand : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Tools";
+        public override void SetStaticDefaults() => ItemID.Sets.DisableAutomaticPlaceableDrop[Type] = true;
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.LivingWoodWand);
+
+            Item.tileWand = ModContent.ItemType<Bogwood>();
+            Item.createTile = ModContent.TileType<LivingBogleaf_Tile>();
+        }
+    }
+}
