a44e616ac7201837c87cf6b278b5d2b686f7cd1d
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2026-07-19 22:20:05 -0500

    Update DesertDjinn_Djinnado.cs

--- _Content/Desert/___PreHardmode/NPCs/__BossDesertDjinn/DesertDjinn_Djinnado.cs
+++ _Content/Desert/___PreHardmode/NPCs/__BossDesertDjinn/DesertDjinn_Djinnado.cs
@@ -1,3 +1,9 @@
+using AAModClassic.UI.World;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -19,6 +25,8 @@ namespace AAModClassic._Content.Desert.___PreHardmode.NPCs.__BossDesertDjinn
             Projectile.tileCollide = false;
             Projectile.penetrate = -1;
             Projectile.timeLeft = 1200;
+            if (WorldTypeSystem.IsWorldOptionEnabled(AAWorldOption.Unofficial))
+                Projectile.alpha = 255;
         }
 
         public override void AI()
@@ -39,6 +47,49 @@ namespace AAModClassic._Content.Desert.___PreHardmode.NPCs.__BossDesertDjinn
                     Projectile.localAI[0] = 1f;
                 }
             }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            if (WorldTypeSystem.IsWorldOptionEnabled(AAWorldOption.Unofficial))
+            {
+                float upTime = 50f;
+                float counter = Projectile.ai[0];
+                float opacity = MathHelper.Clamp(counter / 10f, 0f, 1f);
+                if (counter > upTime - 40f)
+                    opacity = MathHelper.Lerp(1f, 0f, (counter - (upTime - 40f)) / 40f);
+
+                Point point5 = Projectile.Center.ToTileCoordinates();
+                Collision.ExpandVertically(point5.X, point5.Y, out var topY, out var bottomY, 15, 15);
+                float topWorld = (topY + 1) * 16 + 8;
+                float bottomWorld = (bottomY - 1) * 16 + 8;
+                float distance = bottomWorld - topWorld;
+                Texture2D texture = TextureAssets.Projectile[ProjectileID.SandnadoFriendly].Value;
+                float baseRotation = -(float)Math.PI / 50f * counter;
+                float distTravelled = 0f;
+                float jump = 5.1f;
+                Color baseColor = new(212, 192, 100);
+                for (float i = (int)bottomWorld; i > (int)topWorld; i -= jump)
+                {
+                    distTravelled += jump;
+                    float travelRatio = distTravelled / distance;
+                    float rotationOffset = distTravelled * ((float)Math.PI * 2f) / -20f;
+                    float scale = travelRatio - 0.15f;
+                    Color drawColor = Color.Lerp(Color.Transparent, baseColor, travelRatio * 2f);
+                    if (travelRatio > 0.5f)
+                    {
+                        drawColor = Color.Lerp(Color.Transparent, baseColor, 2f - travelRatio * 2f);
+                    }
+                    drawColor.A = (byte)(drawColor.A * 0.5f);
+                    drawColor *= opacity;
+                    Vector2 drawPos = new Vector2(Projectile.Center.X, i) - Main.screenPosition;
+                    Main.EntitySpriteDraw(texture, drawPos, null, drawColor, baseRotation + rotationOffset, texture.Size() * 0.5f, 1f + scale, SpriteEffects.None);
+                }
+
+                Main.EntitySpriteDraw(TextureAssets.Projectile[Projectile.type].Value, new Vector2(Projectile.Center.X, Projectile.position.Y) - Main.screenPosition, null, Color.White, Projectile.rotation, TextureAssets.Projectile[Projectile.type].Size() * 0.5f, 1, SpriteEffects.None);
+            }
+
+            return !WorldTypeSystem.IsWorldOptionEnabled(AAWorldOption.Unofficial);
         }
     }
 }
