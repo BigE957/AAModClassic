1a6014fa5c568df405d70ac2baa684ecbce02ca1
Big E <evanmorrison42@gmail.com>
2026-07-23 15:07:54 -0400

    + Furniture Refactor: Tables

--- _Content/Void/_PostMoonlord/Items/Tiles/Decoration/DoomFurniture/DoomTable_Tile.cs
+++ _Content/Void/_PostMoonlord/Items/Tiles/Decoration/DoomFurniture/DoomTable_Tile.cs
@@ -1,13 +1,8 @@
-using System;
+using AAModClassic.Utilities;
 using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
-using Terraria.Enums;
-using Terraria.Localization;
-using Terraria.ModLoader;
-using Terraria.ObjectData;
-using Terraria.DataStructures;
 using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace AAModClassic._Content.Void._PostMoonlord.Items.Tiles.Decoration.DoomFurniture
 {
@@ -15,26 +10,8 @@ namespace AAModClassic._Content.Void._PostMoonlord.Items.Tiles.Decoration.DoomFu
 	{
 		public override void SetStaticDefaults()
 		{
-			Main.tileSolidTop[Type] = true;
-			Main.tileFrameImportant[Type] = true;
-			Main.tileNoAttach[Type] = true;
-			Main.tileTable[Type] = true;
-			Main.tileLavaDeath[Type] = true;
-			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
-			TileObjectData.newTile.Origin = new Point16(1, 1);
-			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
-			TileObjectData.newTile.HookCheckIfCanPlace = new PlacementHook(new Func<int, int, int, int, int, int, int>(Chest.FindEmptyChest), -1, 0, true);
-			TileObjectData.newTile.AnchorInvalidTiles = new int[] { 127 };
-			TileObjectData.newTile.StyleHorizontal = true;
-			TileObjectData.newTile.LavaDeath = false;
-			TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
-			TileObjectData.addTile(Type);
-			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
-			LocalizedText name = CreateMapEntryName();
-			// name.SetDefault("Doom Table");
-            AddMapEntry(new Color(200, 0, 0), name);
+            this.SetUpTable(ModContent.ItemType<DoomTable>());
             DustType = ModContent.DustType<Dusts.DoomDust>();
-            AdjTiles = new int[] { TileID.Tables };
 
         }
 		
--- _Unreleased/Content/LostKeep/World/Tiles/Furniture/Keep/KeepTable_Tile.cs
+++ _Unreleased/Content/LostKeep/World/Tiles/Furniture/Keep/KeepTable_Tile.cs
@@ -1,12 +1,6 @@
-using System;
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.DataStructures;
-using Terraria.Enums;
+using AAModClassic.Utilities;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
-using Terraria.ObjectData;
 
 namespace AAModClassic._Unreleased.Content.LostKeep.World.Tiles.Furniture.Keep;
 
@@ -14,27 +8,8 @@ public class KeepTable_Tile : ModTile
 {
 	public override void SetStaticDefaults()
 	{
-		//IL_0113: Unknown result type (might be due to invalid IL or missing references)
-		Main.tileSolidTop[Type] = true;
-		Main.tileFrameImportant[Type] = true;
-		Main.tileNoAttach[Type] = true;
-		Main.tileTable[Type] = true;
-		Main.tileLavaDeath[Type] = true;
-		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
-		TileObjectData.newTile.Origin = new Point16(1, 1);
-		TileObjectData.newTile.CoordinateHeights = new int[2] { 16, 16 };
-		TileObjectData.newTile.HookCheckIfCanPlace = new PlacementHook((Func<int, int, int, int, int, int, int>)Chest.FindEmptyChest, -1, 0, true);
-		TileObjectData.newTile.AnchorInvalidTiles = new int[1] { 127 };
-		TileObjectData.newTile.StyleHorizontal = true;
-		TileObjectData.newTile.LavaDeath = false;
-		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
-		TileObjectData.addTile((int)Type);
-		AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
-		LocalizedText val = CreateMapEntryName();
-		// val.SetDefault("Keep Table");
-		AddMapEntry(new Color(30, 150, 12), val);
-		base.DustType = DustID.Stone;
-		base.AdjTiles = new int[1] { 14 };
+        this.SetUpTable(ModContent.ItemType<KeepTable>());
+        base.DustType = DustID.Stone;
 	}
 
 	public override void NumDust(int i, int j, bool fail, ref int num)
