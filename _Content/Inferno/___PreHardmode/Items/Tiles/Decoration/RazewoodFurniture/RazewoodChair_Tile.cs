ecbabffa9d1792d537a44afd396394226e2439bb
Big E <evanmorrison42@gmail.com>
2026-07-23 15:53:01 -0400

    + Furniture Refactor: Chairs

--- _Content/Inferno/___PreHardmode/Items/Tiles/Decoration/RazewoodFurniture/RazewoodChair_Tile.cs
+++ _Content/Inferno/___PreHardmode/Items/Tiles/Decoration/RazewoodFurniture/RazewoodChair_Tile.cs
@@ -1,10 +1,9 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.Enums;
+using AAModClassic.Utilities;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.ObjectInteractions;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
-using Terraria.ObjectData;
 
 namespace AAModClassic._Content.Inferno.___PreHardmode.Items.Tiles.Decoration.RazewoodFurniture
 {
@@ -12,32 +11,23 @@ namespace AAModClassic._Content.Inferno.___PreHardmode.Items.Tiles.Decoration.Ra
     {
         public override void SetStaticDefaults()
         {
-            Main.tileFrameImportant[Type] = true;
-            Main.tileNoAttach[Type] = true;
-            Main.tileLavaDeath[Type] = true;
-            
-            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
-            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 18 };
-            TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
-            TileObjectData.newTile.StyleWrapLimit = 2;
-            TileObjectData.newTile.StyleMultiplier = 2;
-            TileObjectData.newTile.StyleHorizontal = true;
-            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
-            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
-            TileObjectData.addAlternate(1);
-            TileObjectData.addTile(Type);
-            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsChair);
-            LocalizedText name = CreateMapEntryName();
-            // name.SetDefault("Razewood Chair");
-            AddMapEntry(new Color(205, 62, 12), name);
+            this.SetUpChair(ModContent.ItemType<RazewoodChair>());
             DustType = ModContent.DustType<Dusts.RazewoodDust>();
-            TileID.Sets.DisableSmartCursor[Type] = true;
-            AdjTiles = new int[] { TileID.Chairs };
+
+            VanillaFallbackOnModDeletion = TileID.Chairs;
         }
 
         public override void NumDust(int i, int j, bool fail, ref int num)
         {
             num = fail ? 1 : 3;
         }
+
+        public override void ModifySittingTargetInfo(int i, int j, ref TileRestingInfo info) => FurnitureCommon.ChairSitInfo(i, j, ref info);
+
+        public override bool RightClick(int i, int j) => FurnitureCommon.ChairRightClick(i, j);
+
+        public override void MouseOver(int i, int j) => FurnitureCommon.ChairMouseOver(i, j, ModContent.ItemType<RazewoodChair>());
+
+        public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) => settings.player.IsWithinSnappngRangeToTile(i, j, PlayerSittingHelper.ChairSittingMaxDistance);
     }
 }
--- _Content/Mire/___PreHardmode/Items/Tiles/Decoration/BogwoodFurniture/BogwoodChair_Tile.cs
+++ _Content/Mire/___PreHardmode/Items/Tiles/Decoration/BogwoodFurniture/BogwoodChair_Tile.cs
@@ -1,10 +1,9 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.Enums;
+using AAModClassic.Utilities;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.ObjectInteractions;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
-using Terraria.ObjectData;
 
 namespace AAModClassic._Content.Mire.___PreHardmode.Items.Tiles.Decoration.BogwoodFurniture
 {
@@ -12,31 +11,23 @@ namespace AAModClassic._Content.Mire.___PreHardmode.Items.Tiles.Decoration.Bogwo
 	{
 		public override void SetStaticDefaults()
 		{
-			Main.tileFrameImportant[Type] = true;
-			Main.tileNoAttach[Type] = true;
-			Main.tileLavaDeath[Type] = true;
-			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
-			TileObjectData.newTile.CoordinateHeights = new int[]{ 16, 18 };
-			TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
-			TileObjectData.newTile.StyleWrapLimit = 2; //not really necessary but allows me to add more subtypes of chairs below the example chair texture
-			TileObjectData.newTile.StyleMultiplier = 2; //same as above
-			TileObjectData.newTile.StyleHorizontal = true;
-			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
-			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight; //allows me to place example chairs facing the same way as the player
-			TileObjectData.addAlternate(1); //facing right will use the second texture style
-			TileObjectData.addTile(Type);
-			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsChair);
-			LocalizedText name = CreateMapEntryName();
-			// name.SetDefault("Bogwood Chair");
-			AddMapEntry(new Color(20, 0, 100), name);
-			DustType = ModContent.DustType<Dusts.BogwoodDust>();
-			TileID.Sets.DisableSmartCursor[Type] = true;
-			AdjTiles = new int[]{ TileID.Chairs };
-		}
+            this.SetUpChair(ModContent.ItemType<BogwoodChair>());
+            DustType = ModContent.DustType<Dusts.BogwoodDust>();
+
+            VanillaFallbackOnModDeletion = TileID.Chairs;
+        }
 
 		public override void NumDust(int i, int j, bool fail, ref int num)
 		{
 			num = fail ? 1 : 3;
-		}
+        }
+
+        public override void ModifySittingTargetInfo(int i, int j, ref TileRestingInfo info) => FurnitureCommon.ChairSitInfo(i, j, ref info);
+
+        public override bool RightClick(int i, int j) => FurnitureCommon.ChairRightClick(i, j);
+
+        public override void MouseOver(int i, int j) => FurnitureCommon.ChairMouseOver(i, j, ModContent.ItemType<BogwoodChair>());
+
+        public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) => settings.player.IsWithinSnappngRangeToTile(i, j, PlayerSittingHelper.ChairSittingMaxDistance);
     }
 }
