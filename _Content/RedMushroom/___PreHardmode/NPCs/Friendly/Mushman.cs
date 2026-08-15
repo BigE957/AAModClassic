e4e524197af9e9dcdc9f16849146e370c5fdccf0
虞悖「Yu Bell」 <73875008+zlzhaidou@users.noreply.github.com>
2026-08-15 20:12:08 +0800

    fix

--- _Content/RedMushroom/___PreHardmode/NPCs/Friendly/Mushman.cs
+++ _Content/RedMushroom/___PreHardmode/NPCs/Friendly/Mushman.cs
@@ -497,10 +497,10 @@ namespace AAModClassic._Content.RedMushroom.___PreHardmode.NPCs.Friendly
             shop.Add(ItemID.SuperManaPotion, Condition.DownedMoonLord);
 
             //TODO: Give these text
-            shop.Add<GrandHealingPotion>([new((LocalizedText)null, () => AADowned.DownedAncient)]);
-            shop.Add<GrandManaPotion>([new((LocalizedText)null, () => AADowned.DownedAncient)]);
+            shop.Add<GrandHealingPotion>(AAConditions.DownedAnyLateAncient);
+            shop.Add<GrandManaPotion>(AAConditions.DownedAnyLateAncient);
 
-            shop.Add<TheBigOne>([new((LocalizedText)null, () => AADowned.DownedSAncient)]);
+            shop.Add<TheBigOne>(AAConditions.DownedAnySuperancient);
 
             shop.Register();
         }
